3ee865cf0f5cb0375f7238db64779a968e88d214
Owen Shepherd <owen@owen.cafe>
2023-06-30 22:30:07 +0200

    feat: Add 02

---
+++ 02-twice-as-fast-as-asm/loop-1.c
@@ -0,0 +1,20 @@
+#include <stdbool.h>
+#include <stdint.h>
+
+static
+int to_add[256] = {
+  ['s'] = 1,
+  ['p'] = -1,
+};
+
+int run_switches(const uint8_t *restrict input) {
+  int res = 0;
+  while (true) {
+    const uint8_t c = *input++;
+    if (c == '\0') {
+      return res;
+    } else {
+      res += to_add[(int) c];
+    }
+  }
+}
